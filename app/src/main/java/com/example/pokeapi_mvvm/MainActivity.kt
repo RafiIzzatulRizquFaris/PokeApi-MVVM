863fb8ea37fd4cd0833c3b28ac0dca41e5830c36
quifar <risqufaris@gmail.com>
2020-07-26 21:11:13 +0700

    Update MainActivity.kt
    add adapter and implement MVVM

--- app/src/main/java/com/example/pokeapi_mvvm/MainActivity.kt
+++ app/src/main/java/com/example/pokeapi_mvvm/MainActivity.kt
@@ -3,10 +3,37 @@ package com.example.pokeapi_mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var pokemonViewModel: PokemonViewModel
+    private lateinit var adapter: PokemonAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        adapter = PokemonAdapter {}
+        adapter.notifyDataSetChanged()
+
+        rv_pokemon.layoutManager = GridLayoutManager(this, 2)
+        rv_pokemon.adapter = adapter
+
+
+        pokemonViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(PokemonViewModel::class.java)
+        pokemonViewModel.setListPokemon()
+        pokemonViewModel.getListPokemon().observe(this, Observer { pokemon ->
+            if (pokemon == null){
+                Log.e("Pokemon", "was on null")
+            }else {
+                adapter.setAdapter(pokemon)
+                Log.e("Pokemon", pokemon.toString())
+            }
+        })
+
     }
 }
